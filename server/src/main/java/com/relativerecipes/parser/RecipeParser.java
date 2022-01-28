06ca4511a447d2ad035204a85cb3b0c36b375144
robraymullen <robraymullen@gmail.com>
2022-01-28 21:46:44 +0000

    Traverse the ld JSON with DFS Looking for magic strings and expecting certain json structure just won't be reliable. Every website seems to use different structures or none at all. This is a simple DFS implementation to walk through the json. It doesn't rely on particular values except the one we're actually looking for.

--- server/src/main/java/com/relativerecipes/parser/RecipeParser.java
+++ server/src/main/java/com/relativerecipes/parser/RecipeParser.java
@@ -2,7 +2,10 @@ package com.relativerecipes.parser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.json.JSONArray;
@@ -34,34 +37,22 @@ public class RecipeParser {
 		
 		//Parse JSON-ld data in head
 		doc = Jsoup.parse(content);
-		Element head = doc.head();
 		RecipeData recipeData = new RecipeData();
-		recipeData  = getJsonLD(head, recipeData);
-		System.out.println(recipeData);
+		Element head = doc.head();
+		Elements jsonLd = head.getElementsByAttributeValue("type", "application/ld+json");
+		if (!jsonLd.isEmpty()) {
+			String data = jsonLd.get(0).data();
+			extractAllJSONld(getJSONRecipeData(data), recipeData);
+		}
 		
 		if (!recipeData.isComplete()) {
 			//Parse twitter metadata in head
 			recipeData = getTwitterMetadata(head, recipeData);
-			System.out.println(recipeData.name + " \n"+recipeData.description+" \n"+recipeData.imageUrl);
-			
-			//Extract ingredients in body
-			String ingredients = getIngredientsFromBody(doc.body());
-			
-			//Extract method in body
-			String method = getMethodFromBody(doc.body());
+//			System.out.println(recipeData.name + " \n"+recipeData.description+" \n"+recipeData.imageUrl);
 		}
+		System.out.println(recipeData);
 		return null;
 		
-	}
-	
-	private String getMethodFromBody(Element body) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private String getIngredientsFromBody(Element body) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	private RecipeData getTwitterMetadata(Element head, RecipeData recipeData) {
@@ -80,6 +71,54 @@ public class RecipeParser {
 			}
 		});
 		return recipeData;
+	}
+	
+	private JSONObject getJSONRecipeData(String data) {
+		try {
+			JSONObject json = new JSONObject(data);
+			return jsonTraversal(json);
+		} catch (Exception e) {
+			try {
+				JSONArray jsonArray = new JSONArray(data);
+				return jsonArrayTraversal(jsonArray);
+			} catch (Exception f) {
+				return null;
+			}
+		}
+	}
+	
+	private JSONObject jsonTraversal(JSONObject json) {
+		JSONObject result = null;
+		Iterator<String> keys = json.keys();
+		if (json.has("recipeIngredient")) {
+			return json;
+		}
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object jsonEntry = json.get(key);
+			if (jsonEntry instanceof JSONObject) {
+				JSONObject traversedResult = jsonTraversal((JSONObject) jsonEntry);
+				return traversedResult;
+			} else if (jsonEntry instanceof JSONArray) {
+				JSONArray jsonArray = (JSONArray) jsonEntry;
+				return jsonArrayTraversal(jsonArray);
+			}
+		}
+		
+		return result;
+	}
+	
+	private JSONObject jsonArrayTraversal(JSONArray jsonArray) {
+		for (int i=0;i<jsonArray.length();i++) {
+			if (jsonArray.get(i) instanceof JSONObject) {
+				JSONObject innerObject = (JSONObject) jsonArray.get(i);
+				JSONObject checkResult = jsonTraversal(innerObject);
+				if (checkResult != null) {
+					return checkResult;
+				}
+			}
+		}
+		return null;
 	}
 
 	private RecipeData getJsonLD(Element head, RecipeData recipeData) throws ParseException {
@@ -127,6 +166,7 @@ public class RecipeParser {
 	}
 	
 	private void extractAllJSONld(JSONObject ld, RecipeData data) {
+		if (ld == null) return;
 		data.description = ld.getString("description");
 		data.ingredients = (List<String>) ld.getJSONArray("recipeIngredient").toList().stream().map((ingredient) -> (String) ingredient).collect(Collectors.toList());
 		data.name = ld.getString("name");
