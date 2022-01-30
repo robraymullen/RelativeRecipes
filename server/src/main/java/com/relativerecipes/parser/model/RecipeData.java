e227ce06e54880a53a4e0e979a9d592428513716
robraymullen <robraymullen@gmail.com>
2022-01-30 17:07:08 +0000

    Some unit tests for the JSON processor A few basic tests for the JSON processor. HTML taken from a few recipe sites. Next is to add tests to check the robustness of the processing against a variety of json layouts

--- server/src/main/java/com/relativerecipes/parser/model/RecipeData.java
+++ server/src/main/java/com/relativerecipes/parser/model/RecipeData.java
@@ -90,4 +90,53 @@ public class RecipeData {
 				+ ", \ningredients=" + ingredients + ", \nimageUrl=" + imageUrl + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((imageUrl == null) ? 0 : imageUrl.hashCode());
+		result = prime * result + ((ingredients == null) ? 0 : ingredients.hashCode());
+		result = prime * result + ((instructions == null) ? 0 : instructions.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecipeData other = (RecipeData) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (imageUrl == null) {
+			if (other.imageUrl != null)
+				return false;
+		} else if (!imageUrl.equals(other.imageUrl))
+			return false;
+		if (ingredients == null) {
+			if (other.ingredients != null)
+				return false;
+		} else if (!ingredients.equals(other.ingredients))
+			return false;
+		if (instructions == null) {
+			if (other.instructions != null)
+				return false;
+		} else if (!instructions.equals(other.instructions))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
