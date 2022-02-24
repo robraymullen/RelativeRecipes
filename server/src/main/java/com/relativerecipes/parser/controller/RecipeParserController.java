7add76822d11b02e32372bfcb240566d06380c8e
robraymullen <robraymullen@gmail.com>
2022-02-24 23:00:01 +0000

    Remove redundant repo import And test github actions

--- server/src/main/java/com/relativerecipes/parser/controller/RecipeParserController.java
+++ server/src/main/java/com/relativerecipes/parser/controller/RecipeParserController.java
@@ -24,9 +24,6 @@ public class RecipeParserController {
 	
 	RestTemplate template;
 	
-	@Autowired
-	RecipeRepository repo;
-	
 	public RecipeParserController(RestTemplateBuilder templateBuilder) {
 		this.template = templateBuilder.build();
 	}
